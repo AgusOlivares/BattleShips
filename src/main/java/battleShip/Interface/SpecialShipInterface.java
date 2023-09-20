6969ff6e816af3fee40cade1785163eb58f78214
Martin Farres <91481417+MartinFarres@users.noreply.github.com>
2023-09-20 00:54:44 -0300

    Update SpecialShipInterface.java

--- src/main/java/battleShip/Interface/SpecialShipInterface.java
+++ src/main/java/battleShip/Interface/SpecialShipInterface.java
@@ -3,6 +3,8 @@ package battleShip.Interface;
 /**
  * Interfaz que almacena las firmas de los metodos, showAbiliy() y showExample().
  * Su función es la de otorgar al jugador una explicación tanto textual como grafica de las habilidades de los barcos especiales
+ * @author Agustin Olivares
+ * @version 1.0, 19/9/2023
  */
 public interface SpecialShipInterface {
 
